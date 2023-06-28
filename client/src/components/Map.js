6f28bade7bd011f033986c0423cde9fcf3885aa7
Racquel Glickman <racquel.glickman@gmail.com>
2023-06-27 21:22:30 -0400

    changed coordinate marker loading to new function onLoad

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useMemo } from 'react'
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import React, { useState, useEffect } from 'react'
+import { GoogleMap, Marker, useLoadScript, InfoWindow } from "@react-google-maps/api";
 import './homePage.css'
 import 'world-countries-capitals'
 import Geocode from 'react-geocode'
@@ -12,45 +12,78 @@ function Map({ trip }) {
 
   const [center, setCenter] = useState()
   const [coordinates, setCoordinates] = useState([])
+  const [mapRef, setMapRef] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  const [infoWindowData, setInfoWindowData] = useState();
 
-  useEffect(() => {
-      Geocode.fromAddress(trip.location).then(
-        (response) => {
-          const { lat, lng } = response.results[0].geometry.location;
-          // console.log(lat, lng)
-          setCenter({lat: lat, lng: lng})
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
+  // useEffect(() => {
+      
+  // }, [trip]) 
 
-      const geocodeFunction = async (address) => {
-        const response = await Geocode.fromAddress(address)
-        // console.log(response)
+  const onMapLoad = (map) => {
+    setMapRef(map);
+
+    Geocode.fromAddress(trip.location).then(
+      (response) => {
         const { lat, lng } = response.results[0].geometry.location;
         // console.log(lat, lng)
-        setCoordinates(coordinates => [...coordinates, {lat:lat, lng:lng}])
+        setCenter({lat: lat, lng: lng})
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+    const geocodeFunction = async (address) => {
+      const response = await Geocode.fromAddress(address)
+      console.log(response)
+      const { lat, lng } = response.results[0].geometry.location;
+      console.log(lat, lng)
+      setCoordinates(coordinates => [...coordinates, {lat:lat, lng:lng}])
+  }
+
+    const geocodeActivities = async () => {
+      for (let i = 0; i < trip.activities.length; i++) {
+        console.log(trip.activities[i].address)
+        await geocodeFunction(trip.activities[i].address)
+        console.log('promise is resolved')
+      }
+      console.log('ALL activities were geocoded')
     }
 
-      const geocodeActivities = async () => {
-        for (let i = 0; i < trip.activities.length; i++) {
-          // console.log(trip.activities[i].address)
-          await geocodeFunction(trip.activities[i].address)
-          // console.log('promise is resolved')
-        }
-        // console.log('ALL activities were geocoded')
-      }
+    geocodeActivities();
 
-      geocodeActivities()
+  };
 
-        
-  }, [trip]) 
+  const handleMarkerClick = (id, lat, lng, address) => {
+    mapRef?.panTo({ lat, lng });
+    setInfoWindowData({ id, address });
+    setIsOpen(true);
+  };
+
+  function onDeleteActivity() {
+    console.log('pass this down to re-set coordinates without that one')
+  }
 
 
   // console.log(coordinates)
   const activityMarkers = coordinates.map((coord) => {
-    return <Marker key={uuid()} position={coord}/>
+    return <Marker 
+              key={uuid()} 
+              position={coord} 
+              // onClick={() => handleMarkerClick(id, coord['lat'], coord['lng'])}
+            >
+              {/* {isOpen && infoWindowData?.id === ind && (
+                  <InfoWindow
+                    onCloseClick={() => {
+                      setIsOpen(false);
+                    }}
+                  >
+                    <h3>{infoWindowData.name}</h3>
+                  </InfoWindow>
+              )} */}
+          </Marker>
+    
   })
 
   const { isLoaded } = useLoadScript({
@@ -65,6 +98,7 @@ function Map({ trip }) {
                   mapContainerClassName="googleMap"
                   center={center}
                   zoom={6}
+                  onLoad={onMapLoad}
               >
                 {/* <Marker position={center} /> */}
                 {activityMarkers}
